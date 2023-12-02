d926613085cbfc61500ec6e8af223a1a32583152
Michael Totaro <mtotaro@ncsu.edu>
2023-12-02 16:04:36 -0500

    Adding files

---
+++ delete_tables.sql
@@ -0,0 +1,17 @@
+--Statements to delete tables if something goes wrong.
+
+DROP TABLE Employee;
+
+DROP TABLE Operates;
+
+DROP TABLE Airport;
+
+DROP TABLE WorksOn;
+
+DROP TABLE Flight;
+
+DROP TABLE Plane;
+
+DROP TABLE Passenger;
+
+DROP TABLE GoesOn;
